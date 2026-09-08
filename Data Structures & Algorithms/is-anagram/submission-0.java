7e73062736faa4f2eb7197ba2275a74da92b5f8e
JunHyun <102897968+Jhyun02@users.noreply.github.com>
2026-09-08 21:26:52 +0900

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            sMap.put(c, sMap.getOrDefault(c,0)+1);
+        }
+        
+        for(int i=0; i<t.length(); i++){
+            char c = t.charAt(i);
+            tMap.put(c, tMap.getOrDefault(c,0)+1);
+        }
+
+        return sMap.equals(tMap);
+
+    }
+}
---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int diff = target - nums[i];
+            if(map.containsKey(diff)){
+                int prevIndex = map.get(diff);
+
+                return new int[]{prevIndex,i};
+            }
+
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
